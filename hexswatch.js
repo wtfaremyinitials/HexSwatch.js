400b419dfa39b6510259851c66224c7f74ec5757
Will Franzen <wtfranzen@gmail.com>
2014-01-12 02:49:08 -0600

    Working first version with demos

--- hexswatch.js
+++ hexswatch.js
@@ -7,7 +7,11 @@ window.hexswatch = ({
 	'markup': '<span style="background-color: $1; color: $1; border: 1px solid gray; border-radius: 3px">__</span>',
 	
 	'addSwatches': function(data) {		
-		return data.replace(/(#[0-9a-f]{6})/i, '$1 ' + window.hexswatch.markup);
+		return data.replace(/(#[0-9a-f]{6})/ig, '$1 ' + window.hexswatch.markup);
+	},
+	
+	'replaceWithSwatches': function(data) {		
+		return data.replace(/(#[0-9a-f]{6})/ig, window.hexswatch.markup);
 	}
 	
 });
